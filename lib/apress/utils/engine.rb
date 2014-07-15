3d724c6f1eaff5581fe790be8b0bd9c5e1bd7c9d
bibendi <bibendi@bk.ru>
2014-07-15 17:32:55 +0600

    fix autoload lib path

--- lib/apress/utils/engine.rb
+++ lib/apress/utils/engine.rb
@@ -5,7 +5,7 @@ require 'action_dispatch/routing/mapper'
 module Apress
   module Utils
     class Engine < ::Rails::Engine
-      config.autoload_paths += Dir["#{config.root}/lib/**/"]
+      config.autoload_paths += Dir["#{config.root}/lib/"]
 
       config.after_initialize do
         ::ActionDispatch::Routing::Mapper.send :include, ::Apress::Utils::Extensions::ActionDispatch::Routing::Mapper
