260dd2b258103c7ff1d70b4f9a91cec571f794b1
Michail Merkushin <merkushin.m.s@gmail.com>
2015-11-03 14:53:04 +0500

    fix: misstake in redis specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,18 +5,20 @@ require 'combustion'
 
 require 'apress/utils'
 
+require "redis"
+require "mock_redis"
+redis = MockRedis.new
+Redis.current = redis
+
 Combustion.initialize! :all do
   config.perform_caching_queries = true
   config.cache_store = :memory_store
+  config.query_cache_store = -> { redis }
 end
 
 require 'rspec/rails'
 require "pry-debugger"
 require "test_after_commit"
-require "redis"
-require "mock_redis"
-redis = MockRedis.new
-Redis.current = redis
 
 RSpec.configure do |config|
   config.before do
