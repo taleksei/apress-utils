6d702a59386e0798959961fd5e72ef3def9ba2ff
Semyon Pupkov <semen.pupkov@gmail.com>
2016-07-07 12:22:25 +0500

    fix: allow to include standalone module
    if require apress/utils/email_validation it raised error
    NameError: uninitialized constant Apress::Utils

--- lib/apress/utils/email_validation.rb
+++ lib/apress/utils/email_validation.rb
@@ -1,10 +1,15 @@
 # coding: utf-8
 
 # Public: Содержит регулярное выражение необходимое для валидации email.
-module Apress::Utils::EmailValidation
-  EMAIL_REGEXP = /\A[a-z0-9\-_]+(?:\.[a-z0-9\-\._]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z]{2,}\Z/i
+module Apress
+  module Utils
+    module EmailValidation
+      EMAIL_REGEXP = /\A[a-z0-9\-_]+(?:\.[a-z0-9\-\._]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z]{2,}\Z/i
 
-  def self.regexp
-    EMAIL_REGEXP
+      def regexp
+        EMAIL_REGEXP
+      end
+      module_function :regexp
+    end
   end
 end
