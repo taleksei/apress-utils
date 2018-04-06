ffbf04ec9ac5dc87cf620c87405237ef659488b8
Artem Napolskih <napolskih@gmail.com>
2018-04-06 10:55:13 +0500

    feature: rails 4 Flash fixes
    https://github.com/rails/rails/commit/b97e087321f33283d836c5b5964976c88230349a

--- spec/internal/config/routes.rb
+++ spec/internal/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  #
+  get 'spec' => 'application#show'
 end
--- spec/lib/apress/utils/extensions/flash_spec.rb
+++ spec/lib/apress/utils/extensions/flash_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
-class ApplicationController < ActionController::Base; end
+class ApplicationController < ActionController::Base
+  def show
+    redirect_to('', flash: {error: 'error'})
+  end
+end
 
 describe ApplicationController, type: :controller do
-
   it do
     controller.flash[:error] = 'error'
+
+    expect(controller.flash[:error]).to eq 'error'
+    expect(controller.flash.key?(:error)).to be true
+  end
+
+  it do
+    get :show
 
     expect(controller.flash[:error]).to eq 'error'
     expect(controller.flash.key?(:error)).to be true
