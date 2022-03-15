5b91ab86260a198bc20ce54329f44c8af4e9a9c1
Gelani Geliskhanov <gelanigeliskhanov@gmail.com>
2022-03-15 18:46:14 +0700

    fix: correct rails version rules

--- lib/apress/utils/extensions/action_view/helpers/form_patch.rb
+++ lib/apress/utils/extensions/action_view/helpers/form_patch.rb
@@ -4,7 +4,7 @@ module ActionView
   module Helpers
     module FormHelper
       def html_options_for_form(url_for_options, options)
-        if ::Rails::VERSION::MAJOR >= 4.2
+        if ::Rails::VERSION::MAJOR == 4 && ::Rails::VERSION::MINOR >= 2
           wait_text = options[:wait_text].present? ? options[:wait_text] : nil
           disable_period = options[:submit_disable_period].present? ? options.delete(:submit_disable_period) : nil
           options[:onsubmit] = %{formSubmitter.disableButtons(this, #{wait_text.to_json}, #{disable_period.to_json});#{options[:onsubmit].present? ? options[:onsubmit] : ''}}
--- lib/apress/utils/extensions/action_view/helpers/form_tag_patch.rb
+++ lib/apress/utils/extensions/action_view/helpers/form_tag_patch.rb
@@ -4,15 +4,17 @@ module ActionView
   module Helpers
     module FormTagHelper
       def form_tag(url_for_options = {}, options = {}, *parameters_for_url, &block)
-        wait_text = options[:wait_text].present? ? options[:wait_text] : nil
-        disable_period = options[:submit_disable_period].present? ? options.delete(:submit_disable_period) : nil
-        options[:onsubmit] = %{formSubmitter.disableButtons(this, #{wait_text.to_json}, #{disable_period.to_json});#{options[:onsubmit].present? ? options[:onsubmit] : ''}}
+        if rails_less_than_42?
+          wait_text = options[:wait_text].present? ? options[:wait_text] : nil
+          disable_period = options[:submit_disable_period].present? ? options.delete(:submit_disable_period) : nil
+          options[:onsubmit] = %{formSubmitter.disableButtons(this, #{wait_text.to_json}, #{disable_period.to_json});#{options[:onsubmit].present? ? options[:onsubmit] : ''}}
+        end
 
         html_options = html_options_for_form(url_for_options, options, *parameters_for_url)
         html_options["accept-encoding"] = "UTF-8" # TODO Rails - убрать
 
         if block_given?
-          if ::Rails::VERSION::MAJOR == 4 && ::Rails::VERSION::MINOR < 2
+          if rails_less_than_42?
             # https://apidock.com/rails/v4.1.8/ActionView/Helpers/FormTagHelper/form_tag_in_block
             form_tag_in_block(html_options, &block)
           else
@@ -22,6 +24,10 @@ module ActionView
         else
           form_tag_html(html_options)
         end
+      end
+
+      def rails_less_than_42?
+        @rails_less_than_42 ||= ::Rails::VERSION::MAJOR == 4 && ::Rails::VERSION::MINOR < 2
       end
     end
   end
